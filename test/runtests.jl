719f5d10316e63d2d3af0513349695567ba90fe6
Michael Alexander Ewert <mewert99@yahoo.com>
2018-10-23 11:24:00 -0700

    test AxisAlignedBoundingBoxes

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,9 @@
 using Test
 using BinaryTrees
-using Base.Iterators 
+using AxisAlignedBoundingBoxes
+using AxisAlignedBoundingBoxes: Vector3
+
+using Base.Iterators
 
 @testset "traverse" begin
   t = Node(1, Node(-2, Node(-3),Node(3,Node(-4),nothing)), Node(2,nothing,Node(4)))
@@ -13,3 +16,15 @@ using Base.Iterators
   @test search(iseven,t) == -2
 end
 
+
+@testset "AABB" begin
+  a = Vector3(1.0,1,1)
+  o = Vector3(0.0,0,0)
+  @test overlaps(AABB(o,a),AABB(0.5*a,2.0*a)) == true
+  @test overlaps(AABB(o,a),AABB(10.0*a,20.0*a)) == false
+  @test volume(AABB(o,a)) == 1.0
+  @test volume(inflate(AABB(o,a),1.0)) == 3.0^3 
+  @test contains(AABB(o,a),AABB(a*0.5,a*0.75)) == true
+  @test contains(AABB(o,a),AABB(a,2.0*a)) == false
+  @test contains(AABB(o,a),AABB(o,a)) == true
+end
